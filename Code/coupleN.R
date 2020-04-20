422de94b534931ed2028bac9ad7c8c085bdb8920
Phyllis Ju <phyllisju@users.noreply.github.com>
2020-04-20 16:08:41 -0400

    coupleN update

--- Code/coupleN.R
+++ Code/coupleN.R
@@ -3,22 +3,22 @@
 #### email: nju@g.harvard.edu
 #### updated: 20-April 2020
 
+# 
+# library(Rcpp)
+# sourceCpp("computeNweights.cpp")
+# source("coupleMultinomial.R")
 
-library(Rcpp)
-sourceCpp("computeNweights.cpp")
-source("coupleMultinomial.R")
-
-N1 <- 15
-N2 <- 20
-stepsize <- 3
-ksize1 <- 10
-ksize2 <- 13
-g <- 1.02
-n <- 4
+# N1 <- 15
+# N2 <- 20
+# stepsize <- 3
+# ksize1 <- 10
+# ksize2 <- 13
+# g <- 1.02
+# n <- 4
 coupleN <- function(N1, N2, k1,k2, g, n){
-  w <- computeNweights(g = g, N1 =N1, N2  = N2, stepsize = stepsize, ksize1 = ksize1,ksize2 = ksize2, n = n)
+  w <- computeNweights(g = g, N1 =N1, N2  = N2, stepsize = stepsize, ksize1 = k1,ksize2 = k2, n = n)
   index <- coupleMultinomial(p = w[2,],q = w[3,], n = dim(w)[2])
   return(as.integer(w[1,index]))
 }
 
-coupleN(N1, N2, k1, k2, g, n)
+# coupleN(N1, N2, k1, k2, g, n)
