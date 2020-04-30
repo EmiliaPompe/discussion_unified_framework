b0bc2d5d47f061f2fdea5b513e4186eccea24434
Phyllis Ju <phyllisju@users.noreply.github.com>
2020-04-29 22:51:20 -0400

    change notation, ex. L to lag, a to alpha, p to theta, lambda to eta

--- Code/initPara.R
+++ Code/initPara.R
@@ -1,16 +1,20 @@
 V <- V - 1
 ## initialize parmeters 
-### test config
+lag <- 3
+concentration <- 10000
 stepsize <- 1000
-nMCMC <- 600
-nBurn <- nMCMC / 2
-nRepeats <- 100
-L <- 3
-concentration <- 1000
-### production config
-# nMCMC <- 10000
-# nBurn <- 5000
-# nRepeats <- 1000
+### test config
+if(test){
+  nMCMC <- 1000
+  nBurn <- nMCMC / 2
+  nRepeats <- 100
+}else{
+  ### production config
+  nMCMC <- 10000
+  nBurn <- 5000
+  nRepeats <- 50
+}
+
 
 
 ## smaller data config
@@ -18,22 +22,26 @@ concentration <- 1000
 V <- V[1:20,1:4]
 ## numbers of possibilities for each column
 dimV <- dimV[1:4]
+maxN <- 5 * 20 
 
+
+## full data config 
+maxN <- 10**4
 
 dime=as.integer(dimV)
 ## data size 
 n=as.integer(nrow(V))
 ## number of fields
 Hdim=as.integer(ncol(V))
-H <- Hdim[1]
+L <- Hdim[1]
 ## lambda is also eta - labels
-lambda <- sample(n, size = n, replace = T) 
+eta <- sample(n, size = n, replace = T) 
 
 cumdime <- 1 +  c(0,  cumsum(dime)) 
 ## theta - parameter for true record value
 ## frequencies of categories 
-p=as.double(unlist(ALPHA)) ## this is a flat vector, alternatively can store p in an array, but this is what the C file is like
-p <- p[1: sum(dime)]
+theta=as.double(unlist(ALPHA)) ## this is a flat vector, alternatively can store p in an array, but this is what the C file is like
+theta <- theta[1: sum(dime)]
 ## for small dataset need to change the zeros in p 
 # p[p == 0] <- 0.005
 # for (l in 1:H){
@@ -41,7 +49,7 @@ p <- p[1: sum(dime)]
 # }
 
 ## alpha - distorion parameter
-a <- matrix(0.01, nrow = n, ncol = H)
+alpha <- matrix(0.01, nrow = n, ncol = H)
 ## hyperparameter for prior on N 
 g <- 1.02
 ## initial population size 
@@ -49,16 +57,16 @@ N1 <- 2500
 
 
 # find another set of parameters for the coupled gibbs 
-lambda1 <- lambda 
-lambda2 <- sample(n, size = n, replace = T)
+eta1 <- eta 
+eta2 <- sample(n, size = n, replace = T)
 N1 <- N1
 N2 <- N1 + 10 
-a1 <- matrix(0.01, nrow = n, ncol = H)
-a2 <- matrix(0.01, nrow = n, ncol = H)
-p1 <- p 
-p2 <- rep(NA, length(p1))
-for (l in 1:H){
-  p2[cumdime[l] : (cumdime[l+1] -1 ) ] <- gtools::rdirichlet(1, alpha = p1[cumdime[l] : (cumdime[l+1] - 1)] * concentration )[1,]
+alpha1 <- matrix(0.01, nrow = n, ncol = H)
+alpha2 <- matrix(0.01, nrow = n, ncol = H)
+theta1 <- theta 
+theta2 <- rep(NA, length(theta1))
+for (l in 1:L){
+  theta2[cumdime[l] : (cumdime[l+1] -1 ) ] <- gtools::rdirichlet(1, alpha = 1 + theta1[cumdime[l] : (cumdime[l+1] - 1)] * concentration )[1,]
   # print(sum(p2[cumdime[l] : (cumdime[l+1] -1 ) ]))
 }
 
