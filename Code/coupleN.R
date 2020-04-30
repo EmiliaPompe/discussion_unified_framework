8f7a6f777d11f2a2bf3dcd64870ce7cddeb60a5f
Phyllis Ju <phyllisju@users.noreply.github.com>
2020-04-30 13:46:07 -0400

    add a new function to the gibbs update of N

--- Code/coupleN.R
+++ Code/coupleN.R
@@ -16,9 +16,19 @@
 # g <- 1.02
 # n <- 4
 coupleN <- function(N1, N2, k1,k2, g, n){
-  w <- computeNweights(g = g, N1 =N1, N2  = N2, stepsize = stepsize, ksize1 = k1,ksize2 = k2, n = n)
-  index <- couple_multinomial_alt(p = w[2,],q = w[3,], n = dim(w)[2])
-  return(as.integer(w[1,index]))
+  # w <- computeNweights(g = g, N1 =N1, N2  = N2, stepsize = stepsize, ksize1 = k1,ksize2 = k2, n = n)
+  # index <- couple_multinomial_alt(p = w[2,],q = w[3,], n = dim(w)[2])
+  # return(as.integer(w[1,index]))
+  lweights1 <- sapply(1:maxN, function(xx) lfactorial(xx) - lfactorial(xx - k1) + (n+g) * log(xx) )
+  lweights2 <- sapply(1:maxN, function(xx) lfactorial(xx) - lfactorial(xx - k2) + (n+g) * log(xx) )
+  maxlogweights1 <- max(lweights1)
+  lweights1 <- lweights1 - maxlogweights1
+  w1 <- exp(lweights1) / sum(exp(lweights1))
+  maxlogweights2 <- max(lweights2)
+  lweights2 <- lweights2 - maxlogweights2
+  w2 <- exp(lweights2) / sum(exp(lweights2))
+  cp_n_res <- couple_multinomial_alt(p = w1, q = w2, n = maxN)
+  return(cp_n_res)
 }
 
 # coupleN(N1, N2, k1, k2, g, n)
