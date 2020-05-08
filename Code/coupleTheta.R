583640b04280cfb37cfa8a0844c2f4f1fa6fe5bb
Phyllis Ju <phyllisju@users.noreply.github.com>
2020-05-08 09:36:57 -0400

    change theta to a list

--- Code/coupleTheta.R
+++ Code/coupleTheta.R
@@ -2,27 +2,24 @@ sum_notInf <- function(xx){
 	sum(xx[which(is.finite(xx))])
 }
 
-update_theta <- function(p, clustering){
+update_theta <- function(theta, partition, alpha){
   ## for each field
-	cl_log_lik <- compute_loglikelihood_clusters(clustering, p, V, dimV, a)
-	p_propose <- p
-  for (l in 1:H){
+  for (l in 1:L){
   	## current state
-    x <- p[cumdime[l] : (cumdime[l+1] - 1)] 
+    x <- theta[[l]] 
+    cl_log_lik <- compute_loglikelihood_clusters_field(l - 1, partition, x, V, dimV, alpha)
     ## dirichlet proposal
     x_propose <- gtools::rdirichlet(1, alpha = (1 + concentration * x))[1,]
-    p_propose[ cumdime[l] : (cumdime[l+1] - 1)] <- x_propose
+    cl_log_lik_new <- compute_loglikelihood_clusters_field(l - 1, partition, x_propose, V, dimV, alpha)
     ## transition ratio 
     lratio <- log(gtools::ddirichlet(x = x, alpha = (1 + concentratoin * x_propose))) - log(gtools::ddirichlet(x = x_propose, alpha = (1 + concentration * x)))
-    cl_log_lik_new <- compute_loglikelihood_clusters(clustering, p_propose, V, dimV, a)
     ## log accept probability
     laccept <- lratio - sum_notInf(cl_log_lik[,l]) + sum_notInf(cl_log_lik_new[,l])
     if (log(runif(1)) < laccept){
-    	p <- p_propose
-    	cl_log_lik <- cl_log_lik_new
+      theta[[l]] <- x_propose
     }
   }
-  return(p)
+  return(theta)
 }
 
 # clustering1 <- partition1
@@ -31,8 +28,8 @@ update_theta <- function(p, clustering){
 coupleTheta <- function(theta1, theta2, partition1, partition2, alpha1, alpha2){
   for ( l in 1 : L){
     ## first propose from coupled dirichlet
-    x1 <- theta1[cumdime[l] : (cumdime[l+1] - 1)] 
-    x2 <- theta2[cumdime[l] : (cumdime[l+1] - 1)]
+    x1 <- theta1[[l]]
+    x2 <- theta2[[l]]
     cp_dirichlet <- couple_dirichlet(mean1 = (1 / concentration + x1) , mean2 = (1 / concentration + x2), inversescale = 1 / concentration)$xy
     x1_propose <- cp_dirichlet[1:length(x1)]
     x2_propose <- cp_dirichlet[(length(x1) + 1) : (2 * length(x1))]
@@ -46,10 +43,10 @@ coupleTheta <- function(theta1, theta2, partition1, partition2, alpha1, alpha2){
     laccept2 <- lratio2 - sum_notInf(cl_field_loglik2) + sum_notInf(cl_field_loglik2_propose)
     lU <- log(runif(1))
     if (lU < laccept1){
-      theta1[ cumdime[l] : (cumdime[l + 1] -1)]  <- x1_propose
+      theta1[[l]] <- x1_propose
     }
     if (lU < laccept2){
-      theta2[ cumdime[l] : (cumdime[l + 1] -1)]  <- x2_propose
+      theta2[[l]]  <- x2_propose
     }
   }
   return(list(theta1 = theta1, theta2 = theta2))
